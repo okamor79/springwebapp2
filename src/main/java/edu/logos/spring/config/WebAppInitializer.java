ec7ebcc0a24c4e13bec824f8e399210984267c1d
okamor79 <okamor@gmail.com>
2018-05-07 12:35:32 +0300

    bootstrap

--- src/main/java/edu/logos/spring/config/WebAppInitializer.java
+++ src/main/java/edu/logos/spring/config/WebAppInitializer.java
@@ -3,8 +3,10 @@ package edu.logos.spring.config;
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.DispatcherServlet;
 
+import javax.servlet.FilterRegistration;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration;
@@ -22,5 +24,13 @@ public class WebAppInitializer implements WebApplicationInitializer {
         ServletRegistration.Dynamic reg = servletContext.addServlet("dispatcherServlet", new DispatcherServlet(webContext));
         reg.setLoadOnStartup(1);
         reg.addMapping("/");
+
+        CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+        FilterRegistration.Dynamic characterEncoding = servletContext.addFilter("encodingFilter",
+                characterEncodingFilter);
+        characterEncoding.setInitParameter("encoding", "UTF-8");
+        characterEncoding.setInitParameter("forceEncoding", "true");
+        characterEncoding.addMappingForUrlPatterns(null, true, "/*");
+
     }
 }
